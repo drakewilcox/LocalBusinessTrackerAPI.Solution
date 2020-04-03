651f070c1ef5784f60a2605cfc45bbd3efe73050
Drake Wilcox <drakewilcox@gmail.com>
2020-04-03 10:14:12 -0700

    add Location model and Paramaters.

--- LocalBusinessTrackerAPI/Models/LocalBusinessTrackerAPIContext.cs
+++ LocalBusinessTrackerAPI/Models/LocalBusinessTrackerAPIContext.cs
@@ -19,6 +19,6 @@ namespace LocalBusinessTrackerAPI.Models
         //     );
        
 
-        }
+        // }
     }
 }
--- LocalBusinessTrackerAPI/Models/Location.cs
+++ LocalBusinessTrackerAPI/Models/Location.cs
@@ -2,6 +2,13 @@ namespace LocalBusinessTrackerAPI.Models
 {
   public class Location
   {
+    public int LocationId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+    public int Rating { get; set; }
+    public string Hours { get; set; }
+    public string Website { get; set; }
     
   }
 }
