e6bac0349f731aac34403058e4d9df8cf583be31
Drake Wilcox <drakewilcox@gmail.com>
2020-04-03 14:18:00 -0700

    implement swagger.

--- LocalBusinessTrackerAPI/Startup.cs
+++ LocalBusinessTrackerAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace LocalBusinessTrackerAPI
 {
@@ -29,6 +30,11 @@ namespace LocalBusinessTrackerAPI
         {
             services.AddDbContext<LocalBusinessTrackerAPIContext>(opt => opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +49,11 @@ namespace LocalBusinessTrackerAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+             app.UseSwagger();
+               app.UseSwaggerUI(c =>
+            {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             // app.UseHttpsRedirection();
             app.UseMvc();
         }
